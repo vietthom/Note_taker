d227b9103463b2826599aeb67ccd514053b38b97
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-01-17 12:57:22 -0700

    connected api GET request to the server

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const path = require('path');
 const router= express.Router();
 const port = process.env.PORT || 3000;
 
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+//Middleware to render static files
 app.use('/', express.static(path.join(__dirname, '/Develop/public')));
 
-
-
+//Routes to specific pages
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public', 'index.html'));
 });
@@ -15,6 +18,9 @@ router.get('/', (req, res) => {
 router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './Develop/public', 'notes.html'));
 });
+
+//This should get me the 'getNotes' GET request
+router.get('/api/notes', (req, res) => res.json('getNotes'));
 
 app.use('/', router);
 app.use('/notes', router);
