429c83cb352ba9d0c92b5407e6f18d1f3af8cca9
thomle0418 <91095934+thomle0418@users.noreply.github.com>
2022-01-16 22:33:40 -0700

    figured out how to display static files using express.static and how to route the first index.html page!

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app= express();
+const path = require('path');
+const router= express.Router();
+const port = process.env.PORT || 3000;
+
+app.use('/', express.static(path.join(__dirname, '/Develop/public')));
 
 
 
-app.listen(3000);
+router.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public', 'index.html'));
+});
+
+
+app.use('/', router);
+
+app.listen(port);
+
+
+
